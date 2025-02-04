0877343601db9685e9d9cd51061f9d67fd77ab98
guilherme freitas <guilhermefreitasea@gmail.com>
2025-02-04 18:49:50 -0300

    Correção de imagem com o componente <Image> do Next.js

--- src/components/SocialIcon.js
+++ src/components/SocialIcon.js
@@ -1,11 +1,13 @@
 // components/SocialIcon.js
+import Image from 'next/image';
 
 export default function SocialIcon({ href, src, alt }) {
   return (
     <div className="flex justify-center items-center rounded-full size-16 hover:bg-gradient-button hover:scale-[1.2] transition duration-[800ms]">
       <a href={href} target="_blank" rel="noopener noreferrer">
-        <img className="size-8" src={src} alt={alt} />
+        <Image className="size-8" src={src} alt={alt}  width={40} height={40} />
       </a>
     </div>
   );
 }
+  
